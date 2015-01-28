9431d286fcd44e4c6b51c76d07c654e920059050
Svein-Tore Griff With <s.t.g.with@gmail.com>
2015-01-28 14:59:47 +0100

    Initial commit

---
+++ js/columns.js
@@ -0,0 +1,78 @@
+var H5P = H5P || {};
+
+/**
+ * Will render several columns with potentially multiple content instances in each column
+ *
+ * @param {Array} content
+ * @param {int} contentId
+ * @returns {H5P.Columns} Instance
+ */
+H5P.Columns = (function ($) {
+        
+  function C (content, contentId) {
+    if (!(this instanceof H5P.Columns)) {
+      return new H5P.Columns(content, contentId);
+    }
+
+    H5P.QuestionContainer.call(this);
+    
+    var defaults = {
+      columns: [],
+    };
+    this.params = $.extend(true, {}, defaults, content);
+
+    this.columns = new Array();
+    this.$myDom;
+    
+    // Instantiate column instances
+    for (var i = 0; i < this.params.columns.length; i++) {
+      var columnData = this.params.content[i];
+
+      // override content parameters.
+      if (this.params.override.overrideButtons) {
+        // Extend subcontent with the overrided settings.
+        $.extend(columnData.params.behaviour, {
+          enableRetry: this.params.override.overrideRetry,
+          enableSolutionsButton: this.params.override.overrideShowSolutionButton
+        });
+      }
+
+      $.extend(columnData.params, {
+        postUserStatistics: false
+      });
+      
+      this.columns.push(H5P.newRunnable(columnData, contentId));
+    }
+  }
+  
+  C.prototype = Object.create(H5P.QuestionContainer.prototype);
+  C.prototype.constructor = C;
+
+  // Function for attaching the pages to a dom element.
+  C.prototype.attach = function (target) {
+    // TODO: Add code that creates the actual HTML
+    if (typeof(target) === "string") {
+      this.$myDom = $('#' + target);
+    }
+    else {
+      this.$myDom = $(target);
+    }
+    
+    this.$myDom.addClass('h5p-columns');
+
+    // Attach columns
+    for (var i = 0; i < this.columns.length; i++) {
+      var column = this.columns[i];
+      
+      var $columnHolder = $('<div class="h5p-column"></div>');
+
+      column.attach($columnHolder);
+      this.$myDom.append($columnHolder);
+    }
+
+    this.$.trigger('resize');
+    return this;
+  };
+
+  return C;
+})(H5P.jQuery);
